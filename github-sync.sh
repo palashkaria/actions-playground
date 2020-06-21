82c32727a99b02ec5f8e29aba04df4fd823b68e8
Palash <palash@locus.sh>
2020-06-21 18:26:26 +0530

    test copy

---
+++ github-sync.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+UPSTREAM_REPO=$1
+BRANCH_MAPPING=$2
+
+if [[ -z "$UPSTREAM_REPO" ]]; then
+  echo "Missing \$UPSTREAM_REPO"
+  exit 1
+fi
+
+if [[ -z "$BRANCH_MAPPING" ]]; then
+  echo "Missing \$SOURCE_BRANCH:\$DESTINATION_BRANCH"
+  exit 1
+fi
+
+if ! echo $UPSTREAM_REPO | grep '\.git'
+then
+  UPSTREAM_REPO="https://github.com/${UPSTREAM_REPO}.git"
+fi
+
+echo "UPSTREAM_REPO=$UPSTREAM_REPO"
+echo "BRANCHES=$BRANCH_MAPPING"
+
+git config --unset-all http."https://github.com/".extraheader
+git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
+git remote add tmp_upstream "$UPSTREAM_REPO"
+git fetch tmp_upstream
+git remote --verbose
+git push origin "refs/remotes/tmp_upstream/${BRANCH_MAPPING%%:*}:refs/heads/${BRANCH_MAPPING#*:}" --force
+git remote rm tmp_upstream
+git remote --verbose
